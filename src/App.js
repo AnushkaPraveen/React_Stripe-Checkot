abbf7e4d28fbd4415b44d31bb8d0ccc740c213d1
AnushkaPraveen <anushkapraveen98@gmail.com>
2022-06-01 01:32:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import StripeCheckout from 'react-stripe-checkout';
+
 
 function App() {
+
+function handleToken(token,addresses){
+  console.log({token,addresses});
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Stripe Checkout</h1>
+      <StripeCheckout
+        token={handleToken}
+        stripeKey="Publishable key" //insert Publishable key of stripe API keys
+      />
     </div>
   );
 }
